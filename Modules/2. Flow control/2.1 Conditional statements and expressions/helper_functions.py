14d64526fd1fa1943b78849cc410feae279a16b9
gabeflomo <gabeflomo821@gmail.com>
2021-11-05 15:06:56 -0500

    Dictionaries part 1 done

--- Modules/2. Flow control/2.1 Conditional statements and expressions/helper_functions.py
+++
@@ -1,9 +0,0 @@
-from random import randint, choice
-
-def generate_number(range_ = (1,100)):
-    return randint(range_[0], range_[1])
-
-def random_fruit():
-    fruits = ["pomegranate", "apple", "plum", "orange", "kiwi", "pitanga", "pear", "pineapple", "raspberry", "peach", "watermelon" ]
-    return choice(fruits)
-
